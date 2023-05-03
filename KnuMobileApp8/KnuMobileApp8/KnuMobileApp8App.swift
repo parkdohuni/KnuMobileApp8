99998793de37f4e3502bb35c46a60f633d292ecf
deanidh <deanidh1011@gmail.com>
2023-05-03 18:42:32 +0900

    feat: 프로젝트 생성

---
+++ KnuMobileApp8/KnuMobileApp8/KnuMobileApp8App.swift
@@ -0,0 +1,17 @@
+//
+//  KnuMobileApp8App.swift
+//  KnuMobileApp8
+//
+//  Created by Dean on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct KnuMobileApp8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
